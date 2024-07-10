686c2ce4f1abdeb5369bbeb2f4a43e8e040f0815
IsabellFtz <85625355+IsabellFtz@users.noreply.github.com>
2024-07-10 23:01:18 +0200

    Update visualize.jl with comments

--- src/visualize.jl
+++ src/visualize.jl
@@ -2,26 +2,22 @@ module Visualize
 
 using ..Core: Node
 
-export MazeViz, visualize
+export visualize
 
-# Define the MazeViz struct containing the visualization of the maze
-struct MazeViz
-    # Add fields for visualization (empty for now)
-end
 
-# Function to visualize the maze in the terminal
+#Funktion zur Visualisierung von Mazes und gegebenen Pfaden(meistens die Lösungspfade)
 function visualize(nodes::Matrix{Node}, start::Node, goal::Node, path::Vector{Node} = Node[])
     height, width = size(nodes)
     println("Visualizing the maze:")
 
     for y in 1:height
-        # Top border
+        #Visualisierung der horizontalen Wände
         for x in 1:width
             node = nodes[y, x]
             print(node.walls[:top] ? "+---" : "+   ")
         end
         println("+")
-        # Left border and cell content
+        #Visualisierung der vertikalen Wände
         for x in 1:width
             node = nodes[y, x]
             print(node.walls[:left] ? "| " : "  ")
@@ -38,11 +34,42 @@ function visualize(nodes::Matrix{Node}, start::Node, goal::Node, path::Vector{No
         println("|")
     end
 
-    # Bottom border
+    #Die unterste Wand
     for x in 1:width
         print("+---")
     end
     println("+")
 end
 
-end # module Visualize
+function visualize(nodes::Matrix{Node}, start::Node, goal::Node)
+    height, width = size(nodes)
+    println("Visualizing the maze:")
+
+    for y in 1:height
+        for x in 1:width
+            node = nodes[y, x]
+            print(node.walls[:top] ? "+---" : "+   ")
+        end
+        println("+")
+        
+        for x in 1:width
+            node = nodes[y, x]
+            print(node.walls[:left] ? "| " : "  ")
+            if node == start
+                print("Ⓢ ")
+            elseif node == goal
+                print("☆ ")
+            else
+                print("  ")
+            end
+        end
+        println("|")
+    end
+
+    for x in 1:width
+        print("+---")
+    end
+    println("+")
+end
+
+end 
