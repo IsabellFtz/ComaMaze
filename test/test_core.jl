869ec253640d3b13dcbf98004df4017c0f978fcb
IsabellFtz <85625355+IsabellFtz@users.noreply.github.com>
2024-07-10 23:02:27 +0200

    Update test_core.jl without comments

--- test/test_core.jl
+++ test/test_core.jl
@@ -1,22 +1,6 @@
-# test_core.jl
-
-#=using Test
 include("../src/core.jl")
-
-# Explicitly use Core.Node
+using .Core: neighbors
 using .Core: Node
-
-# Your test cases
-height = 5
-width = 5
-nodes = [Node(x, y, false) for y in 1:height, x in 1:width]
-length(nodes) == height * width
-println(nodes[1, 1] isa Node)
-println(nodes)
-=#
-
-include("../src/core.jl")
-using .Core
 
 # Create grid of nodes
 nodes = [Node(x, y, false) for y in 1:5, x in 1:5]
